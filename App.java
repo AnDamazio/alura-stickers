cca02c782d1f8844916f04ff223d929f276d8a79
AnDamazio <damazioandre@hotmail.com>
2022-07-18 12:30:38 -0300

    First class

--- App.java
+++ App.java
@@ -1,11 +1,31 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("Hello, world");
 
         //fazer uma conexão HTTP e buscar os top 250 filmes
 
-        //pegar só os dados que interessam (título, poster, classificação)
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_jy06knz2";
+        URI address = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(address).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
 
-        //exibir e manipular os dados 
+        var parser = new JsonParser();
+        //pegar só os dados que interessam (título, poster, classificação)
+        List<Map<String, String>> movies = parser.parse(body);
+        System.out.println(movies.get(0).get("fullTitle"));
+        System.out.println(movies.get(0).get("image"));
+        System.out.println(movies.get(0).get("imDbRating"));
+        //exibir e manipular os dados
     }
 }
