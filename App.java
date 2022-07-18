3281c8b6f565cb2b25afcca73c8133f6857ccb32
AnDamazio <damazioandre@hotmail.com>
2022-07-18 11:16:55 -0300

    first commit

---
+++ App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+
+        //fazer uma conexão HTTP e buscar os top 250 filmes
+
+        //pegar só os dados que interessam (título, poster, classificação)
+
+        //exibir e manipular os dados 
+    }
+}
